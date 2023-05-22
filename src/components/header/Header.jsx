e8a128657b6a439cb7c60d1d35b2c897b8fedb25
NGABO Prince <53578332+MFASHINGABO@users.noreply.github.com>
2023-05-21 22:38:25 -0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = ()=> {
       <div className="container header__container">
        <h5>Hello I'm</h5>
        <h1>Prince MFASHINGABO</h1>
-       <h5 className="text-light">Ui/Ux | FullStack | Mobile Dev | DevOps</h5>
+       <h5 className="text-light">Ui/Ux | FullStack | Mobile Dev </h5>
        <CTA/>
         <HeaderSocials/>
 
