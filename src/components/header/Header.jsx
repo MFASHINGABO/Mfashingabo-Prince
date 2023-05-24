20d09c285183ce140fc541dc1d7c532886e66e38
NGABO Prince <53578332+MFASHINGABO@users.noreply.github.com>
2023-05-24 02:07:53 -0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/CV-MFASHINGABO_Prince.pdf'
+import ME from '../../assets/prince.jpeg'
 import HeaderSocials from './HeaderSocials'
 
 
